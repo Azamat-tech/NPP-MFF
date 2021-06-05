84d5f25ae80d7fbeef2778bf6b6d13ceff7c8d99
Azamat Zarlykov <azarlykovcz@gmail.com>
2021-06-05 23:19:32 +0200

    update

--- haskell/exercises/week4/exercise.hs
+++ haskell/exercises/week4/exercise.hs
@@ -122,8 +122,8 @@ b) Write a function eval :: Exp -> [(String, Int)] -> Int that evaluates an
 expression given an environment, which is an association list that holds 
 the variables of the variables in the expression.
 -}
-data Op = Minus | Plus | Times
-data Expr = Const Int | Var String | OpExpr Expr Op Expr 
+data Op = Minus | Plus | Times deriving (Show)
+data Expr = Const Int | Var String | OpExpr Expr Op Expr deriving (Show)
 
 opfun :: Op -> (Int -> Int -> Int)
 opfun Minus = (-)
@@ -172,9 +172,120 @@ parse s =
         (expr, rest) = f (words s)
     in expr 
 
+
 {-
 9. Simplifying Expressions
-Write a function simplify :: Exp -> Exp that simplifies an expression by combining constants wherever possible, and by eliminating additions/subtractions with 0 or multiplications with 1.
+Write a function simplify :: Exp -> Exp that simplifies an expression by combining 
+constants wherever possible, and by eliminating additions/subtractions with 0 or 
+multiplications with 1.
 
 For example, 0 + (x + (2 + 3)) should simplify to x + 5.
 -}
+-- simplify :: Expr -> Expr 
+-- simplify OpExpr ((Const a) Plus (Const b)) = Const (a + b)
+
+
+{-
+10. Lists - CLASS WORK
+Pretend that Haskell does not have a built-in list type.
+
+a) Implement lists using an abstract data type DList a. Comparison operators 
+(e.g. ==, /=, >, <) should work on your class and should compare lists lexicographically.
+
+b) Write a function that computes the sum of all values in a DList Int.
+
+c) Write a function that performs a right fold over a DList a.
+-}
+data DList a = Empty | Cons a (DList a) deriving (Ord, Eq, Show)
+
+dSum :: DList Int -> Int
+dSum Empty = 0 
+dSum (Cons x rest) = x + dSum rest
+
+dfoldr :: (b -> a -> a) -> a -> DList b -> a
+dfoldr _ a Empty = a
+dfolr f a (Cons x rest) = f x (dfoldr f a rest)
+
+{-
+11. Natural Numbers
+Pretend that Haskell does not have the built-in types Int and Integer.
+
+a) Implement natural numbers (i.e. non-negative integers) using an abstract
+data type Nat. Comparison operators (e.g. ==, /=, >, <) should work correctly
+on your class.
+
+b) Implement an addition function add :: Nat -> Nat -> Nat.
+
+c) Implement a multiplication function mul :: Nat -> Nat -> Nat.
+-}
+data Nat = Zero | Succ Nat deriving (Eq, Ord, Show)
+
+one = Succ Zero
+two = Succ one
+three = Succ two
+four = Succ three
+
+add :: Nat -> Nat -> Nat
+add Zero x = x
+add (Succ n) y = add n (Succ y)
+
+mul :: Nat -> Nat -> Nat
+mul Zero _ = Zero
+mul (Succ n) y = add (mul n y) y
+
+{-
+12. Pythagorean Triples
+Write a function triples that takes an integer N and returns a list of integer 
+triples (a, b, c) with 0 < a < b < c ≤ N such that a2 + b2 = c2. If two triples 
+are multiples of each other (e.g. (3, 4, 5) and (6, 8, 10)), only the smaller of 
+them (e.g. (3, 4, 5)) should appear in the list.
+
+> triples 15
+
+[(3,4,5),(5,12,13)]
+-}
+firstT :: (Int, Int, Int) -> Int
+firstT (x,_,_) = x
+
+secondT :: (Int, Int, Int) -> Int
+secondT (_,x,_) = x
+
+thirdT :: (Int, Int, Int) -> Int
+thirdT (_,_,x) = x
+
+multiple :: (Int, Int, Int) -> (Int, Int, Int) -> Bool
+multiple x y 
+    | x < y && firstT y `div` firstT x == secondT y `div` secondT x && 
+        secondT y `div` secondT x == thirdT y `div` thirdT x = True
+    | x > y && firstT x `div` firstT y == secondT x `div` secondT y && 
+        secondT x `div` secondT y == thirdT x `div` thirdT y = True
+    | otherwise = False
+
+removeMultiple :: (Int, Int, Int) -> [(Int, Int, Int)] -> [(Int, Int, Int)]
+removeMultiple _ [] = []
+removeMultiple x (y:ys) 
+    | multiple x y = removeMultiple x ys 
+    | otherwise = y : removeMultiple x ys
+        
+check :: [(Int, Int, Int)] -> [(Int, Int, Int)] -> [(Int, Int, Int)] 
+check [] xs = xs
+check (x:xs) ys = if elem x ys then check xs (removeMultiple x ys) else check xs ys
+
+triples :: Int -> [(Int, Int, Int)]
+triples n = let xs = [(a,b,c) | c <- [1..n], b <- [1..c], a <- [1..b] , a^2 + b^2 == c^2]
+            in check xs xs
+
+{-
+13. Words
+Write a function words :: String → [String] that breaks a string into words. 
+For the purposes of this exercise, a word is any sequence of characters that 
+are not spaces. Words may be separated by any number of spaces.
+-}
+
+words1 :: String -> [String]
+words1 [] = []
+words1 text@(char:otherWords)     
+    | isSpace char = words1 otherWords
+    | otherwise = let tempWord = takeWhile (/= ' ') text
+                      tempOther = dropWhile (/= ' ') text
+                  in tempWord : words1 tempOther
