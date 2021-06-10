5e7add93b622d91d088c28561d3dc1bf709a3475
Azamat Zarlykov <azarlykovcz@gmail.com>
2021-06-10 20:09:13 +0200

    update

---
+++ haskell/exercises/week6/exercise.hs
@@ -0,0 +1,87 @@
+import Data.Maybe
+import Data.Char 
+import Data.List
+{-
+2. International Colors
+Here are some association lists with colors in a few languages:
+
+cz_colors :: [(String, String)]
+cz_colors = [("red", "cerveny"), ("blue", "modry"), ("green", "zeleny")]
+
+fr_colors :: [(String, String)]
+fr_colors = [("red", "rouge"), ("blue", "bleu"), ("yellow", "jaune")]
+
+Write a function fr_to_cz :: String -> Just String that maps a color name in 
+French to its equivalent name in Czech, by translating through English if 
+possible. If the name is absent in either dictionary, return Nothing.
+-}
+cz_colors :: [(String, String)]
+cz_colors = [("red", "cerveny"), ("blue", "modry"), ("green", "zeleny")]
+
+fr_colors :: [(String, String)]
+fr_colors = [("red", "rouge"), ("blue", "bleu"), ("yellow", "jaune")]
+
+lookupInFrench :: String -> [(String, String)]-> Maybe String
+lookupInFrench _ [] = Nothing
+lookupInFrench w (x:list) 
+    | snd x == w = Just (fst x) 
+    | otherwise = lookupInFrench w list 
+
+fr_to_cz :: String -> Maybe String
+fr_to_cz french = do
+    e <- lookupInFrench french fr_colors
+    c <- lookup e cz_colors
+    return (c)
+
+{-
+Example -> Given two filenames, read the first file and copy to the second file,
+converting all characters to lowercase
+-}
+convert :: String -> String -> IO ()
+convert from to = do
+    content <- readFile from
+    let updated = map toLower content
+    writeFile to updated
+
+{-
+3. Merging Files
+Write a function merge :: String -> String -> String -> IO () that takes three 
+filenames, which are the names of two input files and one output file. The input 
+files are sorted files of integers, with one integer per line. The function 
+should merge the contents of the files and write them to the output file, 
+again with one integer per line. The files may be larger than available memory.
+-}
+
+-- merge :: String -> String -> String -> IO ()
+-- merge input1 input2 output = do 
+--     content1 <- readFile input1
+--     content2 <- readFile input2
+--     let c1 = filter (/= '\n') content1
+--         c2 = filter (/= '\n') content2
+--         s = sort ( c1 ++ c2 )
+--     writeFile output s
+
+parseToInt :: String -> [Int]
+parseToInt = map read . words
+
+merge :: String -> String -> String -> IO()
+merge input1 input2 outputFile = do
+                content1 <- readFile input1
+                content2 <- readFile input2
+                let content1ListInt = parseToInt content1
+                let content2ListInt = parseToInt content2
+                let merged = sort $ content1ListInt ++ content2ListInt
+                writeFile outputFile . intercalate "\n" . map show $ merged
+                return ()
+
+{-
+4. Permutations
+Write a function permutations :: [a] -> [a] that returns a list of all permutations
+of a given list. Do not assume that the type a implements Eq.
+-}
+-- permutations :: [a] -> [[a]]
+-- permutations [] = []
+-- permutations (x : xs) = permutations xs 
+
+
+-- Questions got harder to solve so I will leave it like this 
